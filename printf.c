241a9ceb669a10da5815eec355c7ff502b44734a
wil-mus <wilfred_musyoka@yahoo.com>
2023-04-23 07:38:01 -0700

    handling the flag characters that a for non custom conversion specifiers

--- printf.c
+++ printf.c
@@ -16,14 +16,32 @@ int _printf(const char *format, ...)
 {
 	int printed_chars = 0;
 	va_list args;
+	int flag_plus = 0;
+	int flag_space = 0;
+	int flag_hash = 0;
 
 	va_start(args, format);
-
+	
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
+			while (*format == '+' || *format == ' ' || *format == '#')
+			{
+				if (*format == '+')
+				{
+					flag_plus = 1;
+				}
+				else if (*format == ' ')
+				{
+					flag_space = 1;
+				}
+				else if (*format == '#')
+				{
+					flag_hash = 1;
+				}
+			}
 			switch (*format)
 			{
 				case 'c':
@@ -31,11 +49,34 @@ int _printf(const char *format, ...)
 					printed_chars++;
 					break;
 				case 's':
+					{
+
+						if (flag_plus)
+						{
+							_putchar('+');
+							printed_chars++;
+						}
+						else if (flag_space)
+						{
+							_putchar(' ');
+							printed_chars++;
+						}
+					}
 					printed_chars += write(1, va_arg(args, char *), 0);
 					break;
 				case 'S':
 					{
 						char *s = va_arg(args, char *);
+						if (flag_plus)
+						{
+							_putchar('+');
+							printed_chars++;
+						}
+						else if (flag_space)
+						{
+							_putchar(' ');
+							printed_chars++;
+						}
 						while (*s != '\0')
 						{
 							if (*s < ' ' || *s >= 127)
@@ -53,15 +94,44 @@ int _printf(const char *format, ...)
 						break;
 					}
 				case 'u':
+					{
+						if (flag_plus)
+						{
+							_putchar('+');
+							printed_chars++;
+						}
+						else if (flag_space)
+						{
+							_putchar(' ');
+							printed_chars++;
+						}
+					}
 					printed_chars += _putunsigned(va_arg(args, unsigned int));
 					break;
 				case 'o':
+					if (flag_hash)
+					{
+						_putchar('0');
+						printed_chars++;
+					}
 					printed_chars += _putoctal(va_arg(args, unsigned int));
 					break;
 				case 'x':
+					if (flag_hash)
+					{
+						_putchar('0');
+						_putchar('x');
+						printed_chars += 2;
+					}
 					printed_chars += _puthex(va_arg(args, unsigned int), 0);
 					break;
 				case 'X':
+					if (flag_hash)
+					{
+						_putchar('0');
+						_putchar('X');
+						printed_chars += 2;
+					}
 					printed_chars += _puthex(va_arg(args, unsigned int), 1);
 					break;
 				case '%':
