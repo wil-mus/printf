92b80db183583243455018b04fe1c505d51bbd3c
wil-mus <wilfred_musyoka@yahoo.com>
2023-04-23 00:49:39 -0700

    add the printf.c file

---
+++ printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _printf - output of the produce according to format
+ *
+ * @format: character string
+ * @...: The optional arguments
+ */
+
+int _printf(const char *format, ...)
+{
+	int printed_chars = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					printed_chars++;
+					break;
+				case 's':
+					printed_chars += write(1, va_arg(args, char *), 0);
+					break;
+				case '%':
+					_putchar('%');
+					printed_chars++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(*format);
+					printed_chars += 2;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			printed_chars++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (printed_chars);
+}
