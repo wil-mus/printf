f90ca4988404eac1b8f5cfab2b1cea1d90a351f8
bontlenkoale1 <bontlenkoale20@gmail.com>
2023-04-25 10:17:17 -0700

    task1.c

--- main.h
+++ main.h
@@ -69,8 +69,13 @@ int gets_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int gets_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
+
+/*Function to print string in reverse*/
 int print_pointer(va_list types, char buffer[],	int flags, int width, int precision, int size);
+
+/* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[], int flags, int width, int precision, int size);
+
 int print_rot13(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_reverse(va_list types, char buffer[], int flags, int width, int precision, int size);
 
@@ -88,9 +93,5 @@ int handle_write_char(char c, char buffer[], int flags, int width, int precision
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
-
-long int convert_size_number(long int num, int size);
-long int convert_size_unsgnd(unsigned long int num, int size);
-
 
 #endif
---
+++ task1.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddarg.h>
+
+/**
+ * main - handle the following conversion specifiers
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int found = 2015;
+	int curr = 2022;
+
+	int found  2015, curr = 2022
+
+	printf("%d\n", found);
+	printf("%i\n", curr);
+
+	return (0);
+}
