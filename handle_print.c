fc791522d75ffef81180ce8c68e328f1341e5cae
bontlenkoale1 <bontlenkoale20@gmail.com>
2023-04-25 08:37:57 -0700

    all gitted files

--- handle_print.c
+++ handle_print.c
@@ -19,13 +19,12 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
-	fmt_t fmt_type[] =    	
-	{
+	fmt_t fmt_type[] ={
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+		{'r', print_reverse}, {'R', print_rot13}, {'\0', NULL}
 	};
 	for (i = 0; fmt_type[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_type[i].fmt)
